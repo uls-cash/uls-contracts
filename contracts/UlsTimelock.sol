9cb3fa3ba1b6372232cf200540effb011f569d5e
an-ivannikov <info@ivannikov.pro>
2023-11-18 16:23:51 +0400

    up

--- contracts/UlsTimelock.sol
+++ contracts/UlsTimelock.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.23;
 import "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract UlsTimelock is TimelockController {
-    uint256 private _minDelay = 60; // TODO 3 * 24 * 60 * 60 = 259200 (3 days)
-    address[] private _proposers = [0x8b0Aba3973D4cA0c120e85178FDD9c2f8F6fb251]; // TODO
-    address[] private _executors = [0x8b0Aba3973D4cA0c120e85178FDD9c2f8F6fb251]; // TODO
+    uint256 private _minDelay = 3 * 24 * 60 * 60;
+    address[] private _proposers = [0x3b7B23c1a883a7038E681948fb5FACa51fc03474];
+    address[] private _executors = [0x3b7B23c1a883a7038E681948fb5FACa51fc03474];
 
     address private _admin = address(0);
 
